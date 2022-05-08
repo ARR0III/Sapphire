76c8a2e1ae31f88e858c6e015e05698324c68b26
ARR0III <36902548+ARR0III@users.noreply.github.com>
2022-05-08 14:49:21 +0700

    Add files via upload

---
+++ crypt.c
@@ -0,0 +1,192 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include <string.h>
+
+#define ENCRYPT 1
+#define DECRYPT 0
+
+#define BUFFER_SIZE 2048
+
+typedef struct {
+  uint8_t key [BUFFER_SIZE];
+  uint8_t data[BUFFER_SIZE];
+  uint8_t mix [BUFFER_SIZE * 6];
+} CRYPT_CTX;
+
+uint32_t temp = 0;
+uint32_t n = 0;
+
+void init(uint8_t * key, size_t klen) {
+  size_t i;
+  
+  for (i = 0; i < klen; ++i) {
+    temp += key[i] * (i + 1);
+  }
+}
+
+int ascii(int c) {
+  if ((c >= 0x00 && c <= 0x1F) || (c == 0x7F)) {
+    return -1;
+  }
+
+  return c;
+}
+
+int punisher(uint8_t * in, uint8_t * out, size_t ilen) {
+  size_t i, j;
+ 
+  for (i = 0, j = 0; i < ilen; ++i) {
+    if (in[i] >= 'A' && in[i] <= 'Z') {
+      out[j] = in[i];
+      ++j;
+      ++n;
+    }
+    else
+    if (in[i] >= 'a' && in[i] <= 'z') {
+      out[j] = in[i] - 32;
+      ++j;
+      ++n;
+    }
+    else {
+      continue;
+    }
+
+    if (0 == (n % 6)) {
+      out[j] = ' ';
+      ++j;
+      //++n;
+    }
+
+    if (54 == n) {
+      out[j] = '\n';
+      ++j;
+      n = 0;
+    }
+  }
+  out[j] = '\n';
+  return j;
+}
+
+void crypt(uint8_t * data, uint8_t * key, size_t dlen, size_t klen, int tumbler) {
+  if (NULL == data || NULL == key || 0 == dlen || 0 == klen)
+    return;
+
+  size_t i;
+
+  for (i = 0; i < dlen; ++i) {
+    if (data[i] >= 'a' && data[i] <= 'z')
+      data[i] -= 32;
+
+    if (data[i] >= 'A' && data[i] <= 'Z') {
+
+      temp += key[i    % klen];
+      temp += key[temp % klen];
+
+      temp += (i + 1);
+
+      temp  = (temp % 26) + 'A';
+
+      if (ENCRYPT == tumbler)
+        data[i] = ((data[i]  + temp) % 26) + 'A';
+      else
+        data[i] = (((data[i] - temp) + 26) % 26) + 'A';
+    }
+  }
+}
+
+/* crypt test.txt test.txt.en password 1 */
+
+int main(int argc, char * argv[]) {
+  CRYPT_CTX * ctx = NULL;
+              ctx = (CRYPT_CTX *)malloc(sizeof(CRYPT_CTX));
+
+  if (NULL == ctx) {
+    printf("error: cannot allocate memory!\n");
+    return -2;
+  }
+
+  if (5 != argc) {
+    printf("error: %s [input] [output] [password] [0 or 1]\n", argv[0]);
+    free(ctx);
+    return 0;
+  }
+
+  int tumbler = atoi(argv[argc - 1]);
+  int klen    = strlen(argv[argc - 2]);
+
+  if (klen > BUFFER_SIZE) {
+    klen = 1024;
+  }
+
+  if (tumbler < 0 || tumbler > 1) {
+    printf("error: \"%s\" not number [0..1]!\n", argv[argc - 1]);
+    free(ctx);
+    return -1;
+  }
+
+  FILE * fi = fopen(argv[1], "rb");
+  FILE * fo = fopen(argv[2], "wb");
+
+  if (NULL == fi) {
+    printf("error: file %s not open\n", argv[1]);
+    
+    free(ctx);
+
+    fclose(fi);
+    fclose(fo);
+
+    return -1;
+  }
+
+  if (NULL == fi) {
+    printf("error: file %s not open\n", argv[2]);
+
+    free(ctx);
+
+    fclose(fi);
+    fclose(fo);
+
+    return -1;
+  }
+
+  size_t read;
+  int    ret_s;
+
+  memcpy(ctx->key, argv[argc - 2], klen);
+
+  init(ctx->key, klen);
+
+  while (1) {
+    read = fread(ctx->data, 1, BUFFER_SIZE, fi);
+
+    if (ENCRYPT == tumbler)
+      ret_s = punisher(ctx->data, ctx->mix, read);
+
+    crypt(tumbler ? ctx->mix : ctx->data,
+          ctx->key,
+          tumbler ? ret_s     : read,
+          klen, tumbler);
+    
+    if (fwrite(tumbler ? ctx->mix : ctx->data, 1,
+               tumbler ? read     : ret_s, fo) != tumbler ? read : ret_s) {
+      
+      printf("error writing in output stream!\n");
+      break;
+    }
+
+    fflush(fo);    
+
+    if (read < BUFFER_SIZE)
+      break;
+  }
+
+  fclose(fi);
+  fclose(fo);
+
+  memset(ctx, 0x00, sizeof(CRYPT_CTX));
+
+  free(ctx);
+  return 0;
+}
